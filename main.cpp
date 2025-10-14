c05f99ecf7ea1658ac6b19b64919eb49a4ad0710
ILTODARO <105315298+ILTODARO@users.noreply.github.com>
2025-10-14 23:18:04 +0200

    lolD

--- main.cpp
+++ main.cpp
@@ -1,15 +1,64 @@
 #include <iostream>
 using namespace std;
 
-class Persona{
-    private:
-    string  name, cogn;
-    int     eta;
+class Persona {
+private:
+    string name, cogn;
+    int eta;
 
-    public:
-    Passeggero(string nm, string cgn, int et, bool cond=false)
+public:
+    // --- Costruttore ---
+    // Inizializza gli attributi della classe con i valori passati come argomenti.
+    Persona(string nm, string cgn, int e) {
+        name = nm;
+        cogn = cgn;
+        eta = e;
+    }
 
+    // --- Distruttore Virtuale ---
+    // Buona pratica per classi destinate a essere basi per altre classi (ereditarietà).
+    virtual ~Persona() {}
+
+    // --- Metodi SET ---
+    // Permettono di modificare il valore degli attributi privati dall'esterno.
+    void setName(string nm) {
+        name = nm;
+    }
+
+    void setCogn(string cgn) {
+        cogn = cgn;
+    }
+
+    void setEta(int e) {
+        eta = e;
+    }
+
+    // --- Metodi GET ---
+    // Permettono di leggere il valore degli attributi privati dall'esterno.
+    // 'const' indica che questi metodi non modificano lo stato dell'oggetto.
+    string getName() const {
+        return name;
+    }
+
+    string getCogn() const {
+        return cogn;
+    }
+
+    int getEta() const {
+        return eta;
+    }
+
+    // --- Funzione Virtuale ---
+    // Stampa le informazioni della persona.
+    // 'virtual' permette a questa funzione di essere ridefinita (override)
+    // in eventuali classi derivate.
+    virtual void stampaInformazioni() const {
+        cout << "Nome: " << name << endl;
+        cout << "Cognome: " << cogn << endl;
+        cout << "Eta: " << eta << endl;
+    }
 };
+
 
 
 class Passeggero{
